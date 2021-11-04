83fefb78b0c7aee4f4648435fd0b0cf1114cd74b
Pabliz <36200735+pabliz@users.noreply.github.com>
2021-11-04 20:16:50 +0100

    first commit - photography - brutalist architecture

--- src/script.js
+++ src/script.js
@@ -1,23 +1,33 @@
 import "./style.css";
 import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { TrackballControls } from "three/examples/jsm/controls/TrackballControls";
+import a from './a.jpg'
+import b from './b.jpg'
+import c from './c.jpg'
+import d from './d.jpg'
+import e from './e.jpg'
+import f from './f.jpg'
+
 const canvas = document.querySelector("canvas.webgl")
-const wrapper = document.querySelector(".wrapper")
-const fullscreen = document.querySelector(".fullscreen")
-let a = document.getElementById('a');
-a.play()
+
+let aa = document.getElementById('aa');
 
 // Scene
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0xCACCD2);
 
 // Object
-const geometry = new THREE.BoxGeometry(1.6, 1.6, .9);
-const textureA = new THREE.VideoTexture(a);
+const geometry = new THREE.BoxGeometry(1.6, 1, 1.6);
+const loader = new THREE.TextureLoader();
+const cubeMaterials = [
+    new THREE.MeshBasicMaterial({ map: loader.load(a), transparent: true, side: THREE.DoubleSide }), //right side
+    new THREE.MeshBasicMaterial({ map: loader.load(d), transparent: true, side: THREE.DoubleSide }), //left side
+    new THREE.MeshBasicMaterial({ map: loader.load(c), transparent: true, side: THREE.DoubleSide }), //top side
+    new THREE.MeshBasicMaterial({ map: loader.load(b), transparent: true, side: THREE.DoubleSide }), //bottom side
+    new THREE.MeshBasicMaterial({ map: loader.load(e), transparent: true, side: THREE.DoubleSide }), //front side
+    new THREE.MeshBasicMaterial({ map: loader.load(f), transparent: true, side: THREE.DoubleSide }), //back side
+];
 
-const material = new THREE.MeshBasicMaterial({ color: 0xffffff, map: textureA, transparent: true });
-material.opacity = 0.35;
-const mesh = new THREE.Mesh(geometry, material);
+const mesh = new THREE.Mesh(geometry, cubeMaterials);
 scene.add(mesh);
 
 // Sizes
@@ -29,51 +39,29 @@ const sizes = {
 // Resize
 window.addEventListener('resize', () => {
     // Update canvas sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+    width: window.innerWidth
+    height: window.innerHeight
         // Update camera
     camera.aspect = sizes.width / sizes.height
     camera.updateProjectionMatrix()
-        // Update renderer
+    // Update renderer
     renderer.setSize(sizes.width, sizes.height);
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
-
-// Fullscreen
-const handleFullscreen = () => {
-    const fullScreenElement = document.fullscreenElement || document.webkitFullscreenElement
-    if (!fullScreenElement) {
-        if (wrapper.requestFullscreen()) {
-            wrapper.requestFullscreen()
-        } else if (!wrapper.requestFullscreen()) {
-            wrapper.webkitRequestFullscreen()
-        }
-    } else {
-        if (document.exitFullscreen()) {
-            document.exitFullscreen()
-        } else if (document.webkitExitFullscreen()) {
-            document.webkitExitFullscreen()
-        }
-    }
-}
-
-window.addEventListener('dblclick', handleFullscreen)
-    // Fullscreen button
-fullscreen.addEventListener('click', handleFullscreen)
 
 
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 2.4;
+camera.position.z = 2.2;
 scene.add(camera);
 
 // Controls
-const controls = new OrbitControls(camera, canvas)
+const controls = new TrackballControls(camera, canvas)
 controls.enableDamping = true
     // change controls target
-    // controls.target.x = 0.7
-    // controls.update()
+controls.target.x = 0.8
+controls.update()
     //
 
 // Renderer
@@ -87,16 +75,14 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 // Clock
 const clock = new THREE.Clock()
 
+
 // Animations
 const tick = () => {
     // Clock
     const elapsedTime = clock.getElapsedTime()
-
-    // Controls Update (damping)
+        // Controls Update (damping)
     controls.update()
-
-    // Render
-
+        // Render
     renderer.render(scene, camera);
     window.requestAnimationFrame(tick)
 }
